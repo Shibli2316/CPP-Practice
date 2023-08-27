95423efbc9cc69dcd0d156e9fc2f96f81b4467e4
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-08-27 23:04:20 +0530

    Create armstrong_of_3digit.cpp

---
+++ Basic/armstrong_of_3digit.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+
+    int temp = num;
+
+    int sum = 0;
+    for(int i = 0; i < 3; i++)
+    {
+        int rem = num % 10;
+        num = num / 10;
+        sum = rem*rem*rem + sum;
+    }
+
+    if (temp == sum)
+    {
+        cout << temp << " is an armstrong number.";
+    }
+    else
+    {
+        cout << temp << " is not an armstrong number.";
+    }
+
+    return 0;
+}
