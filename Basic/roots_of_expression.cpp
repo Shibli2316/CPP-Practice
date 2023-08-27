494fe0d6210b16a2726f50289570f70ca3974770
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-08-27 23:03:36 +0530

    Create roots_of_expression.cpp

---
+++ Basic/roots_of_expression.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void findRoots(int a, int b, int c)
+{
+	if (a == 0) {
+		cout << "Invalid";
+		return;
+	}
+
+	int d = b * b - 4 * a * c;
+	double sqrt_val = sqrt(abs(d));
+
+	if (d > 0) {
+		cout << "Roots are real and different \n";
+		cout << (double)(-b + sqrt_val) / (2 * a) << "\n"
+			<< (double)(-b - sqrt_val) / (2 * a);
+	}
+	else if (d == 0) {
+		cout << "Roots are real and same \n";
+		cout << -(double)b / (2 * a);
+	}
+	else
+	{
+		cout << "Roots are complex \n";
+		cout << -(double)b / (2 * a) << " + i"
+			<< sqrt_val / (2 * a) << "\n"
+			<< -(double)b / (2 * a) << " - i"
+			<< sqrt_val / (2 * a);
+	}
+}
+
+int main()
+{
+	int a, b, c;
+    cout << "Enter elements: ";
+    cin >> a >> b >> c;
+	// Function call
+	findRoots(a, b, c);
+	return 0;
+}
+
