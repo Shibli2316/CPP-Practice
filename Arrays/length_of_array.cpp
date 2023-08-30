f025a56647108c9b22cda3ce5dbe4618c05d7e52
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-08-30 23:19:34 +0530

    Create length_of_array.cpp

---
+++ Arrays/length_of_array.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int length_array(int arr[], int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        count++;
+    }
+    cout << count;
+}
+
+int print_array(int arr[], int size)
+{
+    cout << "[ ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "]" << endl;
+}
+
+int main()
+{
+    int size;
+    int *arr = new int[size];
+    cout << "Enter size for the array: ";
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "-> ";
+        cin >> arr[i];
+    }
+
+    cout << "The array is ";
+    print_array(arr, size);
+
+    cout << "The length of the array is ";
+    length_array(arr, size);
+
+    return 0;
+}
+
