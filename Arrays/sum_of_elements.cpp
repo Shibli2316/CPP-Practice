68262e66cf95724bb88b151422b35c187aa28e90
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-08-30 22:59:38 +0530

    Create sum_of_elements.cpp

---
+++ Arrays/sum_of_elements.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int sum_array(int arr[], int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = arr[i] + sum;
+    }
+    cout << sum;
+}
+
+int print_array(int arr[], int size)
+{
+    cout << "[ ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "]" << endl;
+}
+
+int main()
+{
+    int size;
+    int *arr = new int[size];
+    cout << "Enter size for the array: ";
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "-> ";
+        cin >> arr[i];
+    }
+
+    cout << "The array is ";
+    print_array(arr, size);
+
+    cout << "The sum of the elements of the array is ";
+    sum_array(arr, size);
+
+    return 0;
+}
