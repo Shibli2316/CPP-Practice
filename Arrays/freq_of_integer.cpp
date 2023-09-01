3f2e6ef699eae0d8ceb3fa269d9d7c4ba48e2bee
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-09-01 22:22:44 +0530

    Create freq_of_integer.cpp

---
+++ Arrays/freq_of_integer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+int main()
+{
+    int arr[MAX_SIZE];
+    int n, target;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    cout << "Enter the elements of the array:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "--> ";
+        cin >> arr[i];
+    }
+
+    cout << "Enter the number to find its frequency: ";
+    cin >> target;
+
+    int frequency = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            frequency++;
+        }
+    }
+
+    cout << "The frequency of " << target << " in the array is: " << frequency << endl;
+
+    return 0;
+}
