75e8ddc9c3293d7c6afbac120b226f215c159d4a
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2023-12-07 11:10:57 +0530

    questions week 13

--- week13/q4.cpp
+++ week13/q4.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-// User-defined data type
 class MyInteger {
 private:
     int value;
@@ -15,29 +14,24 @@ public:
 
 int main() {
     using namespace std;
-
-    // Example 1: From basic type to user-defined data type
     int intValue;
 
     cout << "Example 1: Basic type to User-defined type" << endl;
     cout << "Enter an integer value: ";
     cin >> intValue;
 
-    // Explicitly convert int to MyInteger
     MyInteger myInt = static_cast<MyInteger>(intValue);
 
     cout << "Original int value: " << intValue << endl;
     cout << "Converted MyInteger value: " << myInt.getValue() << endl;
     cout << endl;
 
-    // Example 2: From user-defined data type to basic type
     int myValue;
 
     cout << "Example 2: User-defined type to Basic type" << endl;
     cout << "Enter a MyInteger value: ";
     cin >> myValue;
 
-    // Explicitly convert MyInteger to int
     int convertedInt = static_cast<int>(myValue);
 
     cout << "Original MyInteger value: " << myValue << endl;
