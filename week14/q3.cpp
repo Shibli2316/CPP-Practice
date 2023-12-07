a55b50534bd8414ff83d7ac08389b9ce049652c2
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2023-12-07 11:28:10 +0530

    q3 w14

--- week14/q3.cpp
+++ week14/q3.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
 
-// Base class
 class Base {
 public:
-    // Virtual function
+
     virtual void show() const {
         cout << "Base class implementation" << endl;
     }
 
-    // Constructor calling virtual function
     Base() {
         cout << "Base class constructor calling virtual function:" << endl;
         show();
         cout << endl;
     }
 
-    // Normal member function calling virtual function
     void callShow() const {
         cout << "Base class member function calling virtual function:" << endl;
         show();
@@ -24,10 +21,8 @@ public:
     }
 };
 
-// Derived class
 class Derived : public Base {
 public:
-    // Override the virtual function
     void show() const override {
         cout << "Derived class implementation" << endl;
     }
